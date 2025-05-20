c9898c37e0c8bbab3925f1545fa22200ca8c0097
Ain-Jeong <ain.jeong@stu.ocsb.ca>
2025-05-20 17:30:50 +0000

    init

--- js/script.js
+++ js/script.js
@@ -1,5 +1,29 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2025 Ain Jeong All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Ain Jeong
+// Created on: May 2025
 // This file contains the JS functions for index.html
+
+'use strict'
+/**
+ * This function gets the weather today
+ * The 'async' is there because it will take time for the internet to return the value
+ */
+async function weatherToday() {
+  // the 'try' is here because the internet may not be working
+  // it is like an 'if ... else' statement'
+  try {
+    const resultJSON = await fetch(
+      'https://openweathermap.org/weather-conditions'
+    )
+    const jsonData = await resultJSON.json()
+    console.log(jsonData)
+    const weatherTodayNumber = jsonData.data[0]
+
+    // output
+    document.getElementById('weather-today').innerHTML =
+      '<p>' + weatherTodayNumber + '</p>'
+  } catch (error) {
+    console.error(error)
+  }
+}
